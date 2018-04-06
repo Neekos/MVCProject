10c15cd186331d01726b7b8a727f85aa972ee37a
Neekos <and1997310366@mail.ru>
2018-04-07 01:15:35 +0700

    save

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php 
+//session_start();
+
+//require 'kint.php';
+//require 'classes/DB.Class.php';
+//ini_set('display_errors', 1);
+//require_once 'application/bootstrap.php';
+	require __DIR__.'/../vendor/autoload.php';
+
+	require __DIR__.'/../bootstrap/app.php'; 
+
+
+	$app->run();
+
+	
+	
--- routes/public.php
+++ routes/public.php
@@ -1,53 +1,4 @@
-<?php
-session_start();
-
-require 'kint.php';
-require 'classes/DB.Class.php';
-ini_set('display_errors', 1);
-//require_once 'application/bootstrap.php';
-
-include __DIR__.'/vendor/autoload.php';
-
-
-$app = new \Slim\App([
-	'settings' =>[
-		'displayErrorDetails' => true,
-	]
-]);
-/*
-$container = $app->getContainer();
-
-$container['view'] = function(){
-	$view = new \Slim\Views\Twig(__DIR__.'/application/views',[
-		'cache' => false
-		]);
-$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-$view->addExtension(new Slim\Views\TwigExtension($container['roter'], $basePath));
-
-return $view;
-
-};
-*/
-
-	// Get container
-	$container = $app->getContainer();
-	
-	$container['db'] = function() {
-		return new PDO('mysql:host=localhost;port=3309;dbname=test', 'root', '');
-	};
-
-	// Register component on container
-	$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__.'/application/views', [
-        'cache' => false
-    ]);
-
-    // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
-
-    return $view;
-	};
+<?php 
 
 	$app->get('/', function ($request, $response) use($app) {
 
@@ -112,6 +63,3 @@ return $view;
 
 
 	});
-
-	$app->run();
-

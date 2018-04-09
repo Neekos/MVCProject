2e2fc516fa567e78bf77b80a40b58f69be5f196f
Андрей Спиридонов <and1997310366@mail.ru>
2018-04-09 14:10:46 +0700

    Создал контроллер для новостей

---
+++ app/controllers/Controller_Articles.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\controllers;
+
+use App\models\Model_Article;
+
+use PDO;
+
+
+class Controller_Articles extends Controller
+{
+	
+	function getAll($request, $respons)
+	{
+		$articles = $this->c->db->query("SELECT * FROM article")->fetchall(PDO::FETCH_CLASS, Model_Article::class);
+		return $this->c->view->render($respons, 'public/news/news.twig', compact('articles'));		
+	}
+
+	function getОne($request, $respons, $args)
+	{
+		$article = $this->c->db->prepare("SELECT * FROM article WHERE id = :id");
+
+			$article->execute([
+					'id' => $args['id'],
+				]);
+
+
+			$article = $article->fetch(PDO::FETCH_OBJ);
+
+
+			if ($article === false) {
+				return $this->render404($respons);
+			}
+
+		return $this->c->view->render($respons, 'public/news/show.twig', compact('article'));		
+	}	
+}

96df3224715e1f6ed40565cd2774db5344a1d07c
Андрей Спиридонов <and1997310366@mail.ru>
2018-04-09 17:03:43 +0700

    Создание отправки данных формы

--- app/controllers/Controller_Signup.php
+++ app/controllers/Controller_Signup.php
@@ -12,4 +12,29 @@
 		{
 			$this->c->view->render($respons, '/public/input/signup.twig');
 		}
+
+		function getSignupConfirm($request, $respons){
+			$this->c->view->render($respons, '/public/input/signupConfirm.twig');
+		}
+
+		function redirectSignupConfirm($request, $respons){
+			return $respons->withRedirect('/signup/confirm/');
+		}
+
+		function actionRegister($request, $respons)
+		{
+			if (isset($_POST['submit'])) {
+				$name = $_POST['name'];
+				$surName = $_POST['surName'];
+				$middleName = $_POST['middleName'];
+				$password = $_POST['password'];
+
+				if (isset($name)) {
+					# code...
+				}
+			}
+
+			$this->c->view->render($respons, '/public/input/signup.twig');
+		}
+
 	}
--- routes/public.php
+++ routes/public.php
@@ -17,6 +17,8 @@
 	$app->group('/signup', function() {
 
 			$this->get('/', Controller_Signup::class .':viewSignup')->setName('signup');
+			$this->get('/confirm/', Controller_Signup::class .':getSignupConfirm');
+			$this->post('/', Controller_Signup::class .':redirectSignupConfirm')->setName('signup');
 	});
 
 	//авторизация
